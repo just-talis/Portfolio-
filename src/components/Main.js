9f35a1fb682951669b94cac59b5a4b7732380b49
Esther Simon <esthersimon540@gmail.com>
2023-04-20 23:07:38 +0100

    feat: adding links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 function Main() {
   return (
-    <div className='w-full h-screen bg-[black]'></div>
-  )
+    <div name="main" className="w-full h-screen bg-[white]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p>Hi, my name is</p>
+        <h1>Esther Simon</h1>
+        <h2>I'm a FrontEnd Developer.</h2>
+        <p>
+          I'm passionate about software engineering and currently learning how
+          to build web apps with HTML, CSS, React, Vue and Javascript. I will
+          love to be part of tech communities and contribute to open-source
+          projects. I'm focused on building responsive frontend web applications.
+        </p>
+        <div>View Work <HiArrowNarrowRight/></div>
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi"
-import { BsFillPersonLinesFill } from "react-icons/bs"
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 
 function NavBar() {
-const [nav, setNav] = useState(false);
-const handleClick = () => setNav(!nav);
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-purple-950 text-gray-100">
@@ -30,7 +30,13 @@ const handleClick = () => setNav(!nav);
       </div>
 
       {/* mobile menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-purple-950 flex flex-col justify-center items-center'}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-purple-950 flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
@@ -39,15 +45,41 @@ const handleClick = () => setNav(!nav);
       </ul>
 
       {/* socials icons */}
-      <div className="flex fixed flex-col top-[35%] left-0">
-<ul>
-    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]">
-        <a className="flex justify-between items-center w-full text-gray-100"
-        href="/">
-            LinkedIn <FaLinkedin size={30}/>
-        </a>
-    </li>
-</ul>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]">
+            <a
+              className="flex justify-between items-center w-full text-gray-100"
+              href="/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#171515]">
+            <a
+              className="flex justify-between items-center w-full text-gray-100"
+              href="/"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-100"
+              href="/"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-purple-950">
+            <a
+              className="flex justify-between items-center w-full text-gray-100"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
