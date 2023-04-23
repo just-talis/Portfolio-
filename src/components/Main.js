2eb00329a0d48a47be304898500c660a5b4a910e
Esther Simon <esthersimon540@gmail.com>
2023-04-23 19:07:47 +0100

    feat: main update

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,16 +6,27 @@ function Main() {
     <div name="main" className="w-full h-screen bg-[white]">
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1>Esther Simon</h1>
-        <h2>I'm a FrontEnd Developer.</h2>
-        <p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-purple-950">
+          Esther Simon
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#636c88]">
+          FRONTEND DEVELOPER.
+        </h2>
+        <p className="text-[#8892b0] max-w-[700px] py-4">
           I'm passionate about software engineering and currently learning how
           to build web apps with HTML, CSS, React, Vue and Javascript. I will
           love to be part of tech communities and contribute to open-source
-          projects. I'm focused on building responsive frontend web applications.
+          projects. I'm focused on building responsive frontend web
+          applications.
         </p>
-        <div>View Work <HiArrowNarrowRight/></div>
+        <div>
+          <button className="hover:bg-[#6fc2b0] hover:border-[#6fc2b0] duration-300 text-white border-2 px-6 py-3 my-2 flex items-center">
+            View Work 
+            <span>
+            <HiArrowNarrowRight  className="ml-3"/>
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
