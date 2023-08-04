65aae2718118300821b8574c6f082e5de9a345d6
Esther Simon <esthersimon540@gmail.com>
2023-08-04 18:52:33 +0100

    folio

--- src/components/About.js
+++ src/components/About.js
@@ -14,17 +14,17 @@ export const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>
-              Hi. I'm Esther Simon, nice to meet you. Please take a look around.
-            </p>
+            <p>Hi, I'm Esther Simon,</p>
+            <p>nice to meet you.</p>
           </div>
           <div>
             <p>
               I am passionate about frontend development and I'm always eager to
               learn more. I am excited to work on projects that challenge me and
-              push me to grow as a developer. Experienced frontend developer
-              skilled in React, TailwindCSS, and JavaScript. Creating beautiful,
-              responsive web applications is my passion.
+              push me to grow as a developer. I am skilled in building feature
+              rich, visually compelling, and high performing web and mobile
+              applications using ReactJs, Javascript and Typescript. Creating
+              beautiful, responsive web applications is my passion.
             </p>
           </div>
         </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import Count from "../assets/projects/Count.jpg";
+import Purple from "../assets/projects/Purple.png";
 
 const Work = () => {
   return (
@@ -9,19 +11,28 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 border-purple-950">
             Work
           </p>
-          <p className="py-4">Check out some of my work</p>
+          <p className="py-6">Check out some of my projects</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div
+          style={{ backgroundImage: `url(${Purple})` }}
+          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
           <div className="shadow-lg shadow-[#fffbfa] group container rounded-md flex justify-center items-center mx-auto content-div">
-            <div>
-              <span></span>
-              <div>
+            <div className="opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                Counter App
+              </span>
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
